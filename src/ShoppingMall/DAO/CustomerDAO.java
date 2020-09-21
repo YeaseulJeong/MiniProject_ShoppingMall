57e3b5d2a360a9dce1395b8e33b6e4fdcf808c17
uko010199@gmail.com <uko010199@gmail.com>
2020-09-21 16:55:09 +0900

    modify customerDAO

--- src/ShoppingMall/DAO/CustomerDAO.java
+++ src/ShoppingMall/DAO/CustomerDAO.java
@@ -9,8 +9,6 @@ import ShoppingMall.VO.CustomerVO;
 
 public class CustomerDAO {
 
-<<<<<<< HEAD
-=======
 	// 입력
 	public boolean addCustomer(CustomerVO customer) {
 		Connection conn = null;
@@ -123,6 +121,4 @@ public class CustomerDAO {
 		}
 		return resultFlag;
 	}
-
->>>>>>> 896a1bfbf2da44caba8c60b2851fc79482ece1a8
 }
